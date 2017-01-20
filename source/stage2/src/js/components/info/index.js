10ead45fccea0c4ec3380e731d4ba5e04371b254
ch_sss <sdhhqb@gmail.com>
2017-01-20 11:06:57 +0800

    stage2 source

---
+++ source/stage2/src/js/components/info/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// info页面
+class Info extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>这里是信息页面。</div>
+    );
+  }
+}
+
+export default Info;
